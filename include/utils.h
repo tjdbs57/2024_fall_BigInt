1073f5e7f4564814183b0ba242936ac792a65974
tjdbs57 <seoyun4376@naver.com>
2024-09-29 15:30:51 +0900

    jsy

--- include/utils.h
+++ include/utils.h
@@ -1,3 +1,9 @@
 #include "config.h"
 
 void bi_new(bigint** x, u32 wordlen);
+void bi_delete(bigint** x);
+
+void bi_assign(bigint** y, bigint* x);
+void bi_gen_rand(bigint**x, int sign, int wordlen);
+void array_rand(word* dst, int wordlen);
+
--- src/utils.c
+++ src/utils.c
@@ -1,12 +1,62 @@
 #include "utils.h"
 
-
+//2.2
 void bi_new(bigint** x, u32 wordlen)
 {
-    //if(*x != NULL)
-    //bi_delete(x);
+    if(*x != NULL)
+        bi_delete(x);
+
     *x = (bigint*)malloc(sizeof(bigint));
     (*x)->sign = NON_NEGATIVE; // 0
     (*x)->wordlen = wordlen;
     (*x)->a = (word*)calloc(wordlen, sizeof(word));
 }
+
+void bi_delete(bigint** x)
+{
+    if(*x == NULL)
+        return;
+#ifdef ZERORIZE
+    array_init((*x)->a, (*x)->wordlen);
+#endif
+
+
+    free((*x)->a);
+    free(*x);
+    *x = NULL;
+}
+
+//2.6
+void bi_assign(bigint** y, bigint* x)
+{
+    if(*y != NULL)
+        bi_delete(y);
+
+    bi_new(y, x->wordlen);
+    (*y)->sign = x->sign;
+    array_copy((*y)->a, x->a, x->wordlen);
+}
+
+//2.7
+void bi_gen_rand(bigint**x, int sign, int wordlen)
+{
+    bi_new(x, wordlen);
+    (*x)->sign = sign;
+    array_rand((*x)->a, wordlen);
+
+    bi_refine(*x);
+}
+
+//byte--> u8
+void array_rand(word* dst, int wordlen)
+{
+    u8* p = (u8*)dst;
+    int cnt = wordlen * sizeof(word);
+    while(cnt > 0)
+    {
+        *p = rand() & 0xff; // rand = DRBG
+        p++;
+        cnt--;
+    }
+ }
+ 
