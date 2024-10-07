2b5f8dba4e4acf3b9b4fc2790e737aa1fc979322
dedunkel <109065363+dedunkel@users.noreply.github.com>
2024-10-07 10:04:36 +0900

    dk

--- include/utils.h
+++ include/utils.h
@@ -7,3 +7,20 @@ void bi_assign(bigint** y, bigint* x);
 void bi_gen_rand(bigint**x, int sign, int wordlen);
 void array_rand(word* dst, int wordlen);
 
+/**
+ * Compare the absolute values of two bigint numbers.
+ * 
+ * @param x Pointer to the first bigint number.
+ * @param y Pointer to the second bigint number.
+ * @return 1 if |x| > |y|, -1 if |x| < |y|, or 0 if |x| == |y|.
+ */
+int compareABS(bigint* x, bigint* y)
+
+/**
+ * Compare two bigint numbers, taking into account their signs.
+ *
+ * @param x Pointer to the first bigint number.
+ * @param y Pointer to the second bigint number.
+ * @return 1 if x > y, -1 if x < y, or 0 if x == y.
+ */
+int compare(bigint* x, bigint* y)
--- src/utils.c
+++ src/utils.c
@@ -7,6 +7,10 @@ void bi_new(bigint** x, u32 wordlen)
         bi_delete(x);
 
     *x = (bigint*)malloc(sizeof(bigint));
+    if (*x == NULL) {
+        printf("Memory allocation erreor : bigint structure\n");
+        exit(EXIT_FAILURE); // 프로그램 종료
+    }
     (*x)->sign = NON_NEGATIVE; // 0
     (*x)->wordlen = wordlen;
     (*x)->a = (word*)calloc(wordlen, sizeof(word));
@@ -62,3 +66,45 @@ void array_rand(word* dst, int wordlen)
     }
  }
  
+int compareABS(bigint* x, bigint* y)
+{
+    int n = x->wordlen;
+    int m = y->wordlen;
+
+    if (n>m){
+        return 1;
+    }
+    else if(n<m){
+        return -1;
+    }
+    else{
+        for(int j=n-1; j=0; j--){
+            if(x->a[j]>y->a[j]){
+                return 1;
+            }
+            else if(x->a[j]<y->a[j]){
+                return -1;
+            }
+        }
+    }
+    return 0;
+}
+
+int compare(bigint* x, bigint* y)
+{
+    if(x->sign == 0 && y->sign ==1){
+        return 1;
+    }
+    else if(x->sign == 1 && y->sign == 0){
+        return -1;
+    }
+    else{
+        int ret = compareABS(x, y);
+        if(x->sign == 0){
+            return ret;
+        }
+        else{
+            return ret*(-1);
+        }
+    } 
+}
