dbee8ab0e7ca6dca0b5955370b1df59fd217a244
tjdbs57 <seoyun4376@naver.com>
2024-10-08 14:47:59 +0900

    format

--- include/test.h
+++ include/test.h
@@ -1,9 +1,3 @@
 #include "config.h"
 
 //declare test functions
-void test_bigint_functions();
-void test_set_by_array();
-void test_set_by_string();
-void test_rand_assign();
-void test_bi_new();
-void test_bi_string();
